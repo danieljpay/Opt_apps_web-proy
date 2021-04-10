e36317aaf84bc062e2dd14f60ed9fe772ee10d68
Daniel Pérez <plupyt_dany@hotmail.com>
2021-04-10 18:02:59 -0500

    index agregado

---
+++ index.php
@@ -0,0 +1,172 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Lector RSS</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="css/heroic-features.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">Feed RSS</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Home
+              <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Content -->
+  <div class="container">
+
+    <!-- Jumbotron Header -->
+    <header class="jumbotron my-4">
+      <h1 class="display-3">¡Bienvenido a nuestro feed RSS!</h1>
+      <p class="lead">En este sitio podrás agregar las urls de las páginas, blogs, videos de los que quieras estar pendientes de sus actualizaciones.</p>
+      <a href="#" class="btn btn-primary btn-lg">Actualizar</a>
+    </header>
+
+    <!-- Urls Input -->
+    <div class="container">
+      <p class="text-center"><strong>Ingresa la Url de donde desees recibir sus actualizaciones:</strong></p>
+      <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="https://www.bbc.com/news" aria-label="Recipient's username" aria-describedby="button-addon2">
+        <div class="input-group-append">
+          <button class="btn btn-outline-primary" type="button" id="button-addon2">Ingresar</button>
+        </div>
+      </div>
+    </div>
+    
+
+    <!-- Page Features -->
+    <div class="row text-center">
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Página 1</h4>
+            <p class="card-text">Descripción de la página.</p>
+          </div>
+          <div class="card-text">
+            Fecha
+          </div>
+          <div class="card-text">
+            <a href="">url de la página</a>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Ver actualizaciones</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Página 2</h4>
+            <p class="card-text">Descripción de la página.</p>
+          </div>
+          <div class="card-text">
+            Fecha
+          </div>
+          <div class="card-text">
+            <a href="">url de la página</a>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Ver actualizaciones</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Página 3</h4>
+            <p class="card-text">Descripción de la página.</p>
+          </div>
+          <div class="card-text">
+            Fecha
+          </div>
+          <div class="card-text">
+            <a href="">url de la página</a>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Ver actualizaciones</a>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-lg-3 col-md-6 mb-4">
+        <div class="card h-100">
+          <img class="card-img-top" src="http://placehold.it/500x325" alt="">
+          <div class="card-body">
+            <h4 class="card-title">Página 4</h4>
+            <p class="card-text">Descripción de la página.</p>
+          </div>
+          <div class="card-text">
+            Fecha
+          </div>
+          <div class="card-text">
+            <a href="">url de la página</a>
+          </div>
+          <div class="card-footer">
+            <a href="#" class="btn btn-primary">Ver actualizaciones</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!-- /.row -->
+
+  </div>
+  <!-- /.container -->
+
+  <!-- Footer -->
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">by Jorge A. Chi, Jimmy N. Ojeda y Daniel J. Pérez &copy; </p>
+    </div>
+    <!-- /.container -->
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+</body>
+
+</html>
