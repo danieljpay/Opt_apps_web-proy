e439862f8437ed9f9dfac0be96fc2c932756da73
Daniel Pérez <plupyt_dany@hotmail.com>
2021-04-17 15:14:14 -0500

    espacio entre variables

--- variables.php
+++ variables.php
@@ -3,6 +3,7 @@
     $usuario = "root";
     $contrasena = "";
     $basedatos = "noticias_opt";
+    
     $queryByDate = "SELECT * FROM `items` ORDER BY Fecha DESC";
     $queryByTitle = "SELECT * FROM `items` ORDER BY Titulo";
     $queryByUrl = "SELECT * FROM `items` ORDER BY itemLink";
