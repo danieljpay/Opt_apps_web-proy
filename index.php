b476c32febb402e62677f3050e356e0c9d926a5c
Daniel Pérez <plupyt_dany@hotmail.com>
2021-05-05 01:11:57 -0500

    Acordeón agregado en explorerNews.php con plantilla

--- index.php
+++ index.php
@@ -62,22 +62,19 @@
 ?>
 
 <head>
+	<meta charset="utf-8">
+	<meta name="viewport"
+		content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+	<title>Lector RSS</title>
+	<link href="./public/assets/favicon.png" rel="icon" type="image/png">
 
-<meta charset="utf-8">
-<meta name="viewport"
-	content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="description" content="">
-<meta name="author" content="">
+	<!-- Bootstrap core CSS -->
+	<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-<title>Lector RSS</title>
-<link href="./public/assets/favicon.png" rel="icon" type="image/png">
-
-<!-- Bootstrap core CSS -->
-<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-<!-- Custom styles for this template -->
-<link href="css/heroic-features.css" rel="stylesheet">
-
+	<!-- Custom styles for this template -->
+	<link href="css/heroic-features.css" rel="stylesheet">
 </head>
 
 <body style=" background-image: url('./public/assets/background.jpg'); background-attachment: fixed; ">
