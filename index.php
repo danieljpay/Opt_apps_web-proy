c0fe403f03af17c7bfb6e5de7161477b525da1ea
Daniel Pérez <plupyt_dany@hotmail.com>
2021-05-24 23:20:22 -0500

    scripts optimizados

--- index.php
+++ index.php
@@ -112,8 +112,8 @@
 	<?php include("components/Footer.html") ?>
 
 	<!-- Bootstrap core JavaScript -->
-	<script src="vendor/jquery/jquery.min.js"></script>
-	<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+	<script defer src="vendor/jquery/jquery.min.js"></script>
+	<script defer src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
